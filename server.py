6678510d60350e67996c58c5a38f1d1caa9ab3d4
adexycity <108495992+adexycity@users.noreply.github.com>
2023-09-13 23:00:37 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-temperature = 25
-if temperature > 30:
-    print("Adewumi is hot")
-    print("Adewumi should drink plenty water")
-elif temperature > 20: #(20, 30)
-    print ("Adewumi is having a nice day !")
-Print("Done !!!")
+# Example: https://docs.aws.amazon.com/apprunner/latest/dg/config-file-examples.html
+version: 1.0
+runtime: python3
+build:
+  commands:
+    build:
+      - pip install -r requirements.txt
+run:
+  runtime-version: 3.8
+  command: uvicorn --host 0.0.0.0 --port 8080 --log-level info main:app
+  network:
+    port: 8080
